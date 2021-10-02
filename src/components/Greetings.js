c418ab2357bba02ddff6734b292f3176e549ee69
Pawel Wozniak <pawel.jan.wozniak@gmail.com>
2021-10-03 00:39:34 +0200

    Methods to fetch data from API were added

--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const API = "https://randomuser.me/api/?nat=us,fr,gb&inc=name,picture";
 
 const Greetings = (props) => {
+
+    const [guests, setGuests] = useState();
     const numPeopleToGuess = props.location.state;
+
+    const handleDataFetch = () => {
+        fetch(`${API}&results=${numPeopleToGuess}`)
+        .then(response => {
+            if(response.ok) return response;
+            throw Error(response.status);
+        })
+        .then(response => response.json())
+        .then(data => setGuests(data.results))
+        .catch(error => console.log(error));
+    }
+    
+    handleDataFetch();
+
     return (
         <div>
             <p>Greetings</p>
-            <p>{numPeopleToGuess}</p>
         </div>
     )
 }
