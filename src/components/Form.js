fd5702c7b78f964e2799ac5ad24cba5938cd042d
Pawel Wozniak <pawel.jan.wozniak@gmail.com>
2021-10-11 00:39:37 +0200

    Check button becomes active when all answers are typed

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,25 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState, useRef } from 'react';
 import { MainContext } from './MainContext';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowAltCircleLeft, faArrowAltCircleRight, faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 
 import Question from './Question';
 
+import '../styles/Form.css';
+
+const back = <FontAwesomeIcon icon={faArrowAltCircleLeft}/>
+const check = <FontAwesomeIcon icon={faCheckCircle}/>
+const forward = <FontAwesomeIcon icon={faArrowAltCircleRight}/>
+
 const Form = () => {
 
-    const {guests} = useContext(MainContext);
+    const { guests } = useContext(MainContext);
     const [answers, setAnswers] = useState(new Array(guests.length).fill(''));
     const [index, setIndex] = useState(0);
+    const backBtnRef = useRef();
+    const checkBtn = useRef();
+    const forwardBtnRef = useRef();
 
     const goBack = () => {
         setIndex(prevValue => prevValue - 1);
@@ -19,20 +31,42 @@ const Form = () => {
     const updateAnswer = (answer) => {
         let currentAnswers = [...answers];
         currentAnswers[index] = answer;
+        if(currentAnswers.indexOf('') !== -1) {
+            checkBtn.current.classList.add('inactive')
+            checkBtn.current.disabled = true;
+        } else {
+            checkBtn.current.classList.remove('inactive')
+            checkBtn.current.disabled = false;
+        }
         setAnswers(currentAnswers);
     }
 
+    useEffect(() => {
+        if(index === 0) {
+            backBtnRef.current.classList.add('inactive');
+            backBtnRef.current.disabled = true;
+        } else if (index === guests.length - 1) {
+            forwardBtnRef.current.classList.add('inactive');
+            forwardBtnRef.current.disabled = true;
+        } else {
+            backBtnRef.current.classList.remove('inactive');
+            backBtnRef.current.disabled = false;
+            forwardBtnRef.current.classList.remove('inactive');
+            forwardBtnRef.current.disabled = false;
+        }
+    },[index]);
+
     return (
-        <div>
+        <div className="form">
+            <h2>Nice to see you again!</h2>
             <Question 
-                answers={answers}
-                goBack={goBack} 
-                goForward={goForward} 
-                guest={guests[index]}
-                index={index}
-                numOfPeopleToGuess={guests.length}
+                currentAnswer={answers[index]} 
+                picture={guests[index].picture.large} 
                 updateAnswer={updateAnswer}
-            />
+            />                       
+            <button ref={backBtnRef} onClick={() => goBack()}>{back}</button>
+            <button ref={forwardBtnRef} onClick={() => goForward()}>{forward}</button>
+            <button className='inactive' disabled={true} ref={checkBtn}>{check}</button>
         </div>
     )
 }
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,49 +1,21 @@
-import React, { useEffect, useRef, useState } from 'react';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowAltCircleLeft, faArrowAltCircleRight, faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import React from 'react';
 
 import '../styles/Question.css';
 
-const back = <FontAwesomeIcon icon={faArrowAltCircleLeft} />
-const check = <FontAwesomeIcon icon={faCheckCircle}/>
-const forward = <FontAwesomeIcon icon={faArrowAltCircleRight} />
 
-const Question = ({answers, goBack, goForward, guest, index, numOfPeopleToGuess, updateAnswer}) => {
 
-    const [inputText,setInputText] = useState(answers[index]);
-    const backBtnRef = useRef();
-    const checkBtn = useRef();
-    const forwardBtnRef = useRef();
+const Question = ({ currentAnswer, picture, updateAnswer }) => {
 
-    const handleOnChange = (event) => setInputText(event.target.value);
-
-    useEffect(() => {
-        setInputText(answers[index]);
-        updateAnswer(inputText);
-        if(index === 0) {
-            backBtnRef.current.classList.add('inactive');
-            backBtnRef.current.disabled = true;
-        } else if (index === numOfPeopleToGuess - 1) {
-            forwardBtnRef.current.classList.add('inactive');
-            forwardBtnRef.current.disabled = true;
-        } else {
-            backBtnRef.current.classList.remove('inactive');
-            backBtnRef.current.disabled = false;
-            forwardBtnRef.current.classList.remove('inactive');
-            forwardBtnRef.current.disabled = false;
-        }
-    },[guest]);
+    const handleOnChange = (event) => {
+        const typedText = event.target.value;
+        updateAnswer(typedText);
+    }
 
     return (
         <div className="question">
-            <h2>Nice to see you again!</h2>
-            <img src={guest.picture.large} alt="face"></img>
+            <img src={picture} alt="face"></img>
             <p>Your answer:</p>
-            <input onChange={handleOnChange} type="text" value={inputText} placeholder='type my name'/>
-            <button ref={backBtnRef} onClick={() => goBack()}>{back}</button>
-            <button ref={forwardBtnRef} onClick={() => goForward()}>{forward}</button>
-            <button ref={checkBtn}>{check}</button>
+            <input onChange={handleOnChange} type="text" value={currentAnswer} placeholder='type my name'/>
         </div>
     )
 }
