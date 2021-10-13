0a4a1c610c1374f623be842f1aedab91ae571c4f
Pawel Wozniak <pawel.jan.wozniak@gmail.com>
2021-10-13 22:13:57 +0200

    Answers are marked as a good or wrong

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,25 +1,50 @@
 import React, { useContext } from 'react';
 import { MainContext } from './MainContext';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRedoAlt } from '@fortawesome/free-solid-svg-icons';
+
+import { useHistory } from 'react-router-dom';
+import '../styles/Results.css';
+
+const tryAgain = <FontAwesomeIcon icon={faRedoAlt}/>
+
 const Results = (props) => {
 
     const answers  = props.location.state;
     const { guests } = useContext(MainContext);
+    const history = useHistory();
 
     const showResults = () => {
-        return guests.map((guest,index) => 
-            <div key={index}>
-                <img alt='face' src={guest.picture.medium}/>
-                <p>{guest.name.first}</p>
-                <p>{answers[index]}</p>
-            </div>
+        return guests.map((guest,index) => {
+            const color = guest.name.first === answers[index]? 'yellowgreen': 'tomato';
+            const borderColor = {borderColor: `${color}`};
+            const pictureSrc = guests.length === 5 
+            ? guest.picture.medium 
+            : guest.picture.thumbnail
+            return (
+                <div key={index}>
+                    <img alt='face' src={pictureSrc} style={borderColor}/>
+                    <p>{guest.name.first}</p>
+                    <p>{answers[index]}</p>
+                </div>
+            )}
         )
+    }
+
+    const handleTryAgainBtn = () => {
+        history.push({
+            pathname: '/',
+        })
     }
 
 
     return (
-        <div>
-            {showResults()}
+        <div className="results">
+            <div className="answers">
+                {showResults()}
+            </div>
+            <button onClick={handleTryAgainBtn}>{tryAgain}</button>
         </div>
     )
 
