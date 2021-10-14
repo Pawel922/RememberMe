27ec75280a2c88c376c1ee057bf9f0b057c85c65
Pawel Wozniak <pawel.jan.wozniak@gmail.com>
2021-10-14 22:14:21 +0200

    Number of good and bad answers is calculated

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,36 +1,50 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { MainContext } from './MainContext';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faRedoAlt } from '@fortawesome/free-solid-svg-icons';
+import { faFrownOpen, faGrin, faRedoAlt } from '@fortawesome/free-solid-svg-icons';
 
 import { useHistory } from 'react-router-dom';
 import '../styles/Results.css';
 
+const badAnswersIcon = <FontAwesomeIcon icon={faFrownOpen}/>
+const goodAnswersIcon = <FontAwesomeIcon icon={faGrin}/>
 const tryAgain = <FontAwesomeIcon icon={faRedoAlt}/>
 
 const Results = (props) => {
 
     const answers  = props.location.state;
+    const [badAnswersScore, setBadAnswersScore] = useState(0);
+    const [goodAnswersScore, setGoodAnswersScore] = useState(0);
     const { guests } = useContext(MainContext);
     const history = useHistory();
 
-    const showResults = () => {
+    const showResults = () => {       
+        let color = '';
         return guests.map((guest,index) => {
-            const color = guest.name.first === answers[index]? 'yellowgreen': 'tomato';
+            guest.name.first === answers[index]
+            ? color = 'yellowgreen'
+            : color = 'tomato'
             const borderColor = {borderColor: `${color}`};
-            const pictureSrc = guests.length === 5 
-            ? guest.picture.medium 
-            : guest.picture.thumbnail
             return (
                 <div key={index}>
-                    <img alt='face' src={pictureSrc} style={borderColor}/>
+                    <img alt='face' src={guest.picture.medium} style={borderColor}/>
                     <p>{guest.name.first}</p>
                     <p>{answers[index]}</p>
                 </div>
-            )}
-        )
+            )
+        })
     }
+
+
+    const countScores = () => {
+        let badAnswers = 0;
+        let goodAnswers = 0;
+        guests.map((guest,index) => (guest.name.first === answers[index]? goodAnswers += 1: badAnswers += 1))
+        setBadAnswersScore(badAnswers);
+        setGoodAnswersScore(goodAnswers);
+    }
+
 
     const handleTryAgainBtn = () => {
         history.push({
@@ -38,9 +52,22 @@ const Results = (props) => {
         })
     }
 
+    useEffect(() => {
+        countScores()
+    },[]);
 
     return (
         <div className="results">
+            <div className="summary">
+                <div>
+                    <p>{badAnswersIcon}</p>
+                    <p>{badAnswersScore}</p>
+                </div>
+                <div>
+                    <p>{goodAnswersIcon}</p>
+                    <p>{goodAnswersScore}</p>
+                </div>
+            </div>
             <div className="answers">
                 {showResults()}
             </div>
