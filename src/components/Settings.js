c06b9b3abd785cac3924be638e700f915fa5f031
Pawel Wozniak <pawel.jan.wozniak@gmail.com>
2021-10-17 00:51:51 +0200

    New elements were added to Settings component

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowAltCircleLeft, faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { faArrowAltCircleLeft, faCheckCircle, faStopwatch } from '@fortawesome/free-solid-svg-icons';
 
 import '../styles/Settings.css';
 import { useHistory } from 'react-router-dom';
 
 const back = <FontAwesomeIcon icon={faArrowAltCircleLeft} />
 const save = <FontAwesomeIcon icon={faCheckCircle} />
+const timer = <FontAwesomeIcon icon={faStopwatch}/>
 
 const Settings = () => {
 
@@ -18,6 +19,15 @@ const Settings = () => {
     return (
         <div className="settings">
             <h2>Settings</h2>
+            <div className="timer">
+                <p className="icon">{timer}</p>
+                <p className="strip">
+                    <p className="indicator"></p>
+                    <p>3s</p>
+                    <p>5s</p>
+                    <p>7s</p>
+                </p>
+            </div>
             <div>
                 <button onClick={handleBackBtn}>{back}</button>
                 <button>{save}</button>
