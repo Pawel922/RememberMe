204b30cbde844e5db10ff9e95293013e492e2f2a
Pawel Wozniak <pawel.jan.wozniak@gmail.com>
2021-10-24 18:56:14 +0200

    Updates

---
+++ precache-manifest.31b4eed7c2f3122dc9c479562261101a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "771d3cc9e84a51814d94",
+    "url": "/RememberMe/static/css/main.0adcbe72.chunk.css"
+  },
+  {
+    "revision": "771d3cc9e84a51814d94",
+    "url": "/RememberMe/static/js/main.8f74b51a.chunk.js"
+  },
+  {
+    "revision": "1865376c9a2090824315",
+    "url": "/RememberMe/static/js/runtime~main.b678dea5.js"
+  },
+  {
+    "revision": "fd52d2fc05661ae9439b",
+    "url": "/RememberMe/static/js/2.c3d93317.chunk.js"
+  },
+  {
+    "revision": "eeced4cc9307e2a6ef8fa0fcf71e31d3",
+    "url": "/RememberMe/index.html"
+  }
+];
