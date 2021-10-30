eed18ac97803459960e822068744d2326bb0d37b
Pawel Wozniak <pawel.jan.wozniak@gmail.com>
2021-10-30 20:32:30 +0200

    Updates

--- precache-manifest.e72d6fd7e2d1f9c0a156e449d07535bd.js
+++ precache-manifest.e72d6fd7e2d1f9c0a156e449d07535bd.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "f24ed8894af41b33c521",
-    "url": "/RememberMe/static/css/main.bc28512e.chunk.css"
+    "revision": "ebcfec282e1d1c636a4b",
+    "url": "/RememberMe/static/css/main.5720fab0.chunk.css"
   },
   {
-    "revision": "f24ed8894af41b33c521",
-    "url": "/RememberMe/static/js/main.8f74b51a.chunk.js"
+    "revision": "ebcfec282e1d1c636a4b",
+    "url": "/RememberMe/static/js/main.8448afc3.chunk.js"
   },
   {
     "revision": "1865376c9a2090824315",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/RememberMe/static/js/2.c3d93317.chunk.js"
   },
   {
-    "revision": "38f6fdd23e805cdcb2c6f4f981db965e",
+    "revision": "f5a02dde952ba4ccb52d343603b06418",
     "url": "/RememberMe/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/RememberMe/precache-manifest.a28736e7ec6ebd0c1b2e229e68a7a27d.js"
+  "/RememberMe/precache-manifest.e72d6fd7e2d1f9c0a156e449d07535bd.js"
 );
 
 workbox.clientsClaim();
